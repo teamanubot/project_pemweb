4b2cf50733e40ca9feb49a73c509f12b033d7789
teamanubot <rivaimunthe02@gmail.com>
2025-07-06 06:21:06 +0700

    Penambahan guards instructor dan admin

--- app/src/config/auth.php
+++ app/src/config/auth.php
@@ -40,6 +40,16 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+
+        'instructor' => [
+            'driver' => 'session',
+            'provider' => 'users',
+        ],
+
+        'admin' => [
+            'driver' => 'session',
+            'provider' => 'users',
+        ],
     ],
 
     /*
