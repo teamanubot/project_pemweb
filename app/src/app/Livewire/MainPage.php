40d55285c40c2bc3ac1e9b1db8508e85bff6f49c
Moocchi <muhammadjarez@gmail.com>
2025-07-09 21:22:06 +0700

    menambahkann livewire MainPage

---
+++ app/src/app/Livewire/MainPage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class MainPage extends Component
+{
+    public function render()
+    {
+        return view('livewire.main-page');
+    }
+}
