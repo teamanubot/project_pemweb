55d90e251b76eef66b85eec12ae39bfbc2f1b7b8
Moocchi <muhammadjarez@gmail.com>
2025-07-12 15:39:32 +0700

    update main page bagian pricing

--- app/src/resources/views/livewire/main-page.blade.php
+++ app/src/resources/views/livewire/main-page.blade.php
@@ -393,8 +393,8 @@
 
         <!-- Section Title -->
         <div class="container section-title" data-aos="fade-up">
-            <h2>Pricing</h2>
-            <p>Necessitatibus eius consequatur ex aliquid fuga eum quidem sint consectetur velit</p>
+            <h2>Pilih Paket</h2>
+            <p>Silahkan pilih paket pembelajaran yang kamu mau!</p>
         </div><!-- End Section Title -->
 
         <div class="container" data-aos="zoom-in" data-aos-delay="100">
@@ -403,64 +403,59 @@
 
                 <div class="col-lg-4">
                     <div class="pricing-item">
+                        <img src="{{ asset('front/assets/img/hero-carousel/Hero-Pics-1.jpg') }}" alt=""
+                            style="width: 100%; margin-bottom: 30px; border-radius: 5px;">
                         <h3>Free Plan</h3>
-                        <div class="icon">
+                        {{-- <div class="icon">
                             <i class="bi bi-box"></i>
-                        </div>
-                        <h4><sup>$</sup>0<span> / month</span></h4>
-                        <ul>
-                            <li><i class="bi bi-check"></i> <span>Quam adipiscing vitae proin</span></li>
-                            <li><i class="bi bi-check"></i> <span>Nec feugiat nisl pretium</span></li>
-                            <li><i class="bi bi-check"></i> <span>Nulla at volutpat diam uteera</span></li>
-                            <li class="na"><i class="bi bi-x"></i> <span>Pharetra massa massa ultricies</span></li>
-                            <li class="na"><i class="bi bi-x"></i> <span>Massa ultricies mi quis hendrerit</span>
-                            </li>
-                        </ul>
-                        <div class="text-center"><a href="#" class="buy-btn">Buy Now</a></div>
-                    </div>
-                </div><!-- End Pricing Item -->
-
-                <div class="col-lg-4">
-                    <div class="pricing-item featured">
-                        <h3>Business Plan</h3>
-                        <div class="icon">
-                            <i class="bi bi-rocket"></i>
-                        </div>
-
-                        <h4><sup>$</sup>29<span> / month</span></h4>
-                        <ul>
-                            <li><i class="bi bi-check"></i> <span>Quam adipiscing vitae proin</span></li>
-                            <li><i class="bi bi-check"></i> <span>Nec feugiat nisl pretium</span></li>
-                            <li><i class="bi bi-check"></i> <span>Nulla at volutpat diam uteera</span></li>
-                            <li><i class="bi bi-check"></i> <span>Pharetra massa massa ultricies</span></li>
-                            <li><i class="bi bi-check"></i> <span>Massa ultricies mi quis hendrerit</span></li>
-                        </ul>
-                        <div class="text-center"><a href="#" class="buy-btn">Buy Now</a></div>
+                        </div> --}}
+                        <h4><sup>Rp</sup>0<span> / bulan</span></h4>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus corrupti eius maiores
+                            delectus dolor commodi labore incidunt explicabo, hic veritatis.
+                        </p>
+                        <div class="text-center"><a href="#" class="buy-btn">Beli Paket</a></div>
                     </div>
                 </div><!-- End Pricing Item -->
 
                 <div class="col-lg-4">
                     <div class="pricing-item">
+                        <img src="{{ asset('front/assets/img/hero-carousel/Hero-Pics-1.jpg') }}" alt=""
+                            style="width: 100%; margin-bottom: 30px; border-radius: 5px;">
+                        <h3>Business Plan</h3>
+                        {{-- <div class="icon">
+                            <i class="bi bi-rocket"></i>
+                        </div> --}}
+
+                        <h4><sup>Rp</sup>29<span> / bulan</span></h4>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus corrupti eius maiores
+                            delectus dolor commodi labore incidunt explicabo, hic veritatis.
+                        </p>
+                        <div class="text-center"><a href="#" class="buy-btn">Beli Paket</a></div>
+                    </div>
+                </div><!-- End Pricing Item -->
+
+                <div class="col-lg-4">
+                    <div class="pricing-item">
+                        <img src="{{ asset('front/assets/img/hero-carousel/Hero-Pics-1.jpg') }}" alt=""
+                            style="width: 100%; margin-bottom: 30px; border-radius: 5px;">
                         <h3>Developer Plan</h3>
-                        <div class="icon">
+                        {{-- <div class="icon">
                             <i class="bi bi-send"></i>
-                        </div>
-                        <h4><sup>$</sup>49<span> / month</span></h4>
-                        <ul>
-                            <li><i class="bi bi-check"></i> <span>Quam adipiscing vitae proin</span></li>
-                            <li><i class="bi bi-check"></i> <span>Nec feugiat nisl pretium</span></li>
-                            <li><i class="bi bi-check"></i> <span>Nulla at volutpat diam uteera</span></li>
-                            <li><i class="bi bi-check"></i> <span>Pharetra massa massa ultricies</span></li>
-                            <li><i class="bi bi-check"></i> <span>Massa ultricies mi quis hendrerit</span></li>
-                        </ul>
-                        <div class="text-center"><a href="#" class="buy-btn">Buy Now</a></div>
+                        </div> --}}
+                        <h4><sup>Rp</sup>49<span> / bulan</span></h4>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus corrupti eius maiores
+                            delectus dolor commodi labore incidunt explicabo, hic veritatis.
+                        </p>
+                        <div class="text-center"><a href="#" class="buy-btn">Beli Paket</a></div>
                     </div>
                 </div><!-- End Pricing Item -->
 
             </div>
 
         </div>
-
     </section><!-- /Pricing Section -->
 
 
