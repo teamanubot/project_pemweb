6995b5bcf1e51192256fdefd7f6824ec536c2b75
Moocchi <muhammadjarez@gmail.com>
2025-07-12 23:29:23 +0700

    perbaikan halaman blog, connect dengan db

--- app/src/resources/views/livewire/blog-page.blade.php
+++ app/src/resources/views/livewire/blog-page.blade.php
@@ -6,7 +6,8 @@
             style="background-image: url(front/assets/img/page-title-bg.jpg);">
             <div class="container position-relative">
                 <h1>Blog</h1>
-                <p>Selamat datang di blog UEU Bootcamp! Temukan berbagai artikel, tips, dan inspirasi seputar dunia teknologi, pemrograman, dan pengalaman seru selama mengikuti bootcamp di Universitas Esa Unggul.</p>
+                <p>Selamat datang di blog UEU Bootcamp! Temukan berbagai artikel, tips, dan inspirasi seputar dunia
+                    teknologi, pemrograman, dan pengalaman seru selama mengikuti bootcamp di Universitas Esa Unggul.</p>
                 <nav class="breadcrumbs">
                     <ol>
                         <li><a href="{{ url('/') }}">Home</a></li>
@@ -18,231 +19,55 @@
 
         <!-- Blog Posts Section -->
         <section id="blog-posts" class="blog-posts section">
-
             <div class="container">
                 <div class="row gy-4">
 
-                    <div class="col-lg-4">
-                        <article class="d-flex flex-column">
+                    @foreach ($posts as $post)
+                        <div class="col-lg-4">
+                            <article class="d-flex flex-column">
 
-                            <div class="post-img">
-                                <img src="{{ asset('front/assets/img/blog/blog-1.jpg') }}" alt="" class="img-fluid">
-                            </div>
+                                <div class="post-img">
+                                    <img src="{{ asset('front/assets/img/blog/blog-1.jpg') }}" alt="" class="img-fluid">
+                                    {{-- Ganti src jika sudah punya kolom gambar --}}
+                                </div>
 
-                            <h2 class="title">
-                                <a href="blog-details.html">Dolorum optio tempore voluptas dignissimos cumque fuga qui
-                                    quibusdam quia</a>
-                            </h2>
+                                <h2 class="title">
+                                    <a href="#">{{ $post->title }}</a>
+                                </h2>
 
-                            <div class="meta-top">
-                                <ul>
-                                    <li class="d-flex align-items-center"><i class="bi bi-person"></i> <a
-                                            href="blog-details.html">John Doe</a></li>
-                                    <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a
-                                            href="blog-details.html"><time datetime="2022-01-01">Jan 1, 2022</time></a>
-                                    </li>
-                                </ul>
-                            </div>
+                                <div class="meta-top">
+                                    <ul>
+                                        <li class="d-flex align-items-center">
+                                            <i class="bi bi-person"></i>
+                                            <a href="#">{{ $post->author->name ?? 'Anonim' }}</a>
+                                        </li>
+                                        <li class="d-flex align-items-center">
+                                            <i class="bi bi-clock"></i>
+                                            <a href="#">
+                                                <time datetime="{{ $post->published_at }}">
+                                                    {{ \Carbon\Carbon::parse($post->published_at)->format('M d, Y H:i:s') }}
+                                                </time>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
 
-                            <div class="content">
-                                <p>
-                                    Similique neque nam consequuntur ad non maxime aliquam quas. Quibusdam animi
-                                    praesentium. Aliquam et laboriosam eius aut nostrum quidem aliquid dicta.
-                                </p>
-                            </div>
+                                <div class="content">
+                                    <p style="text-align: justify;">{{ Str::limit(strip_tags($post->content), 150) }}</p>
+                                </div>
 
-                            <div class="read-more mt-auto align-self-end">
-                                <a href="blog-details.html">Read More</a>
-                            </div>
+                                <div class="read-more mt-auto align-self-end">
+                                    <a href="{{ url($post->slug) }}" target="_blank" rel="noopener">Read More</a>
+                                </div>
 
-                        </article>
-                    </div><!-- End post list item -->
-
-                    <div class="col-lg-4">
-                        <article class="d-flex flex-column">
-
-                            <div class="post-img">
-                                <img src="{{ asset('front/assets/img/blog/blog-2.jpg') }}" alt="" class="img-fluid">
-                            </div>
-
-                            <h2 class="title">
-                                <a href="blog-details.html">Nisi magni odit consequatur autem nulla dolorem</a>
-                            </h2>
-
-                            <div class="meta-top">
-                                <ul>
-                                    <li class="d-flex align-items-center"><i class="bi bi-person"></i> <a
-                                            href="blog-details.html">John Doe</a></li>
-                                    <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a
-                                            href="blog-details.html"><time datetime="2022-01-01">Jan 1, 2022</time></a>
-                                    </li>
-                                </ul>
-                            </div>
-
-                            <div class="content">
-                                <p>
-                                    Incidunt voluptate sit temporibus aperiam. Quia vitae aut sint ullam quis illum
-                                    voluptatum et. Quo libero rerum voluptatem pariatur nam.
-                                </p>
-                            </div>
-
-                            <div class="read-more mt-auto align-self-end">
-                                <a href="blog-details.html">Read More</a>
-                            </div>
-
-                        </article>
-                    </div><!-- End post list item -->
-
-                    <div class="col-lg-4">
-                        <article class="d-flex flex-column">
-
-                            <div class="post-img">
-                                <img src="{{ asset('front/assets/img/blog/blog-3.jpg') }}" alt="" class="img-fluid">
-                            </div>
-
-                            <h2 class="title">
-                                <a href="blog-details.html">Possimus soluta ut id suscipit ea ut. In quo quia et soluta
-                                    libero sit sint.</a>
-                            </h2>
-
-                            <div class="meta-top">
-                                <ul>
-                                    <li class="d-flex align-items-center"><i class="bi bi-person"></i> <a
-                                            href="blog-details.html">John Doe</a></li>
-                                    <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a
-                                            href="blog-details.html"><time datetime="2022-01-01">Jan 1, 2022</time></a>
-                                    </li>
-                                </ul>
-                            </div>
-
-                            <div class="content">
-                                <p>
-                                    Aut iste neque ut illum qui perspiciatis similique recusandae non. Fugit autem
-                                    dolorem labore omnis et. Eum temporibus fugiat voluptate enim tenetur sunt omnis.
-                                </p>
-                            </div>
-
-                            <div class="read-more mt-auto align-self-end">
-                                <a href="blog-details.html">Read More</a>
-                            </div>
-
-                        </article>
-                    </div><!-- End post list item -->
-
-                    <div class="col-lg-4">
-                        <article class="d-flex flex-column">
-
-                            <div class="post-img">
-                                <img src="{{ asset('front/assets/img/blog/blog-4.jpg') }}" alt="" class="img-fluid">
-                            </div>
-
-                            <h2 class="title">
-                                <a href="blog-details.html">Non rem rerum nam cum quo minus. Dolor distinctio deleniti
-                                    explicabo eius exercitationem.</a>
-                            </h2>
-
-                            <div class="meta-top">
-                                <ul>
-                                    <li class="d-flex align-items-center"><i class="bi bi-person"></i> <a
-                                            href="blog-details.html">John Doe</a></li>
-                                    <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a
-                                            href="blog-details.html"><time datetime="2022-01-01">Jan 1, 2022</time></a>
-                                    </li>
-                                </ul>
-                            </div>
-
-                            <div class="content">
-                                <p>
-                                    Aspernatur rerum perferendis et sint. Voluptates cupiditate voluptas atque quae. Rem
-                                    veritatis rerum enim et autem. Saepe atque cum eligendi eaque iste omnis a qui.
-                                </p>
-                            </div>
-
-                            <div class="read-more mt-auto align-self-end">
-                                <a href="blog-details.html">Read More</a>
-                            </div>
-
-                        </article>
-                    </div><!-- End post list item -->
-
-                    <div class="col-lg-4">
-                        <article class="d-flex flex-column">
-
-                            <div class="post-img">
-                                <img src="{{ asset('front/assets/img/blog/blog-5.jpg') }}" alt="" class="img-fluid">
-                            </div>
-
-                            <h2 class="title">
-                                <a href="blog-details.html">Accusamus quaerat aliquam qui debitis facilis
-                                    consequatur</a>
-                            </h2>
-
-                            <div class="meta-top">
-                                <ul>
-                                    <li class="d-flex align-items-center"><i class="bi bi-person"></i> <a
-                                            href="blog-details.html">John Doe</a></li>
-                                    <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a
-                                            href="blog-details.html"><time datetime="2022-01-01">Jan 1, 2022</time></a>
-                                    </li>
-                                </ul>
-                            </div>
-
-                            <div class="content">
-                                <p>
-                                    In itaque assumenda aliquam voluptatem qui temporibus iusto nisi quia. Autem vitae
-                                    quas aperiam nesciunt mollitia tempora odio omnis. Ipsa odit sit ut amet
-                                    necessitatibus. Quo ullam ut corrupti autem consequuntur totam dolorem.
-                                </p>
-                            </div>
-
-                            <div class="read-more mt-auto align-self-end">
-                                <a href="blog-details.html">Read More</a>
-                            </div>
-
-                        </article>
-                    </div><!-- End post list item -->
-
-                    <div class="col-lg-4">
-                        <article class="d-flex flex-column">
-
-                            <div class="post-img">
-                                <img src="{{ asset('front/assets/img/blog/blog-6.jpg') }}" alt="" class="img-fluid">
-                            </div>
-
-                            <h2 class="title">
-                                <a href="blog-details.html">Distinctio provident quibusdam numquam aperiam aut</a>
-                            </h2>
-
-                            <div class="meta-top">
-                                <ul>
-                                    <li class="d-flex align-items-center"><i class="bi bi-person"></i> <a
-                                            href="blog-details.html">John Doe</a></li>
-                                    <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a
-                                            href="blog-details.html"><time datetime="2022-01-01">Jan 1, 2022</time></a>
-                                    </li>
-                                </ul>
-                            </div>
-
-                            <div class="content">
-                                <p>
-                                    Expedita et temporibus eligendi enim molestiae est architecto praesentium dolores.
-                                    Illo laboriosam officiis quis. Labore officia quia sit voluptatem nisi est
-                                    dignissimos totam. Et voluptate et consectetur voluptatem id dolor magni impedit.
-                                    Omnis dolores sit.
-                                </p>
-                            </div>
-
-                            <div class="read-more mt-auto align-self-end">
-                                <a href="blog-details.html">Read More</a>
-                            </div>
-
-                        </article>
-                    </div><!-- End post list item -->
+                            </article>
+                        </div>
+                    @endforeach
 
                 </div>
             </div>
-
-        </section><!-- /Blog Posts Section -->
+        </section>
+        <!-- /Blog Posts Section -->
 
         <!-- Blog Pagination Section -->
         <!-- <section id="blog-pagination" class="blog-pagination section">
