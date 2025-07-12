bdeafbcbf3d8a070d29ccefdfe4d9054ad30fe2c
Moocchi <muhammadjarez@gmail.com>
2025-07-12 22:16:10 +0700

    Menngubah main php untuk pemanggilann di front end

--- app/src/app/Livewire/MainPage.php
+++ app/src/app/Livewire/MainPage.php
@@ -2,12 +2,27 @@
 
 namespace App\Livewire;
 
+use App\Models\Course;
+use App\Models\Department;
+use App\Models\Quiz;
 use Livewire\Component;
+use App\Models\BranchOffice;
 
 class MainPage extends Component
 {
     public function render()
-    {
-        return view('livewire.main-page');
-    }
+{
+    $branchCount = BranchOffice::count();
+    $courseCount = Course::count();
+    $departmentCount = Department::count();
+    $quizCount = Quiz::count();
+
+    return view('livewire.main-page', [
+        'branchCount' => $branchCount,
+        'courseCount' => $courseCount,
+        'departmentCount' => $departmentCount,
+        'quizCount' => $quizCount,
+    ]);
+}
+
 }
